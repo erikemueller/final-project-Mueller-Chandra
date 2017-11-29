aa15412130d8a32cc2c3a2e801d553539ff34b07
Erik Mueller <erikemueller@gmail.com>
2017-11-29 12:44:15 -0600

    fix

--- experimental.py
+++ experimental.py
@@ -1,3 +1,19 @@
+import statsmodels.api as sm
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
+plt.rc("font", size=14)
+from sklearn.linear_model import LogisticRegression
+from sklearn.cross_validation import train_test_split
+import seaborn as sns
+import csv
+from shapely.geometry import Point
+import collections
+
+df = pd.read_csv('CheibubStart.csv')
+gap =pd.read_csv('gap.csv')
+
 groupyear=df.groupby([df['year'], df['Country']]).agg({'count'})
 groupyear.reset_index(level=1, inplace=True)
 groupyear.reset_index(level=0, inplace=True)
@@ -13,4 +29,4 @@ countedCount=pd.merge(gapwork, groupyear, how='outer').dropna(subset=['Populatio
 countedCount=pd.merge(countedCount, df2)
 countedCount['rate'] = np.where(countedCount['count'] < 1, countedCount['Population'], countedCount['count']/(countedCount['Population']/1000000))
 countedCount=countedCount.fillna(0)
-countedCount
+print(countedCount)
